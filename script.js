406193fecf7b788713514c22072a095168019b2c
Deeshma Deepak <deeshmadas@gmail.com>
2024-07-31 13:56:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let num=prompt("Enter the number:")
+if (num>=1 && num<=1000000){
+    console.log("Number:",num)
+}
+else{
+    alert("Invalid number")
+}
+
+let sum=0
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+
+
+}
+console.log("sumof number is:",sum)
